207d850de171bd87d57f18cba2fc990b909007d5
Kovács Dávid <kovacsdavid0831@gmail.com>
2026-06-24 08:06:58 +0200

    Enzopizza weboldal + admin felület

--- admin-cms/vite.config.ts
+++ admin-cms/vite.config.ts
@@ -7,8 +7,7 @@ export default defineConfig({
   server: { port: 5173 },
   build: {
     outDir: "dist",
-    sourcemap: false,
-    commonjsOptions: { include: [] }
+    sourcemap: false
   },
   optimizeDeps: {
     include: [
